32683191c44c6086f4de6cd6b8c362603455ba85
coderpwh <pwhfbc@outlook.com>
2021-11-23 16:35:07 +0800

    Add files via upload

---
+++ chapter2/src/main/java/com/coderpwh/nettty/chapter2/Client/EchoClient.java
@@ -0,0 +1,79 @@
+package com.coderpwh.nettty.chapter2.Client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.InetSocketAddress;
+
+
+/**
+ * Created by pengwenhao on 2021/11/23.
+ */
+@Slf4j
+public class EchoClient {
+
+    private final String host;
+
+    private final int port;
+
+    public EchoClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void start() throws InterruptedException {
+
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .remoteAddress(new InetSocketAddress(host, port))
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+
+                            ch.pipeline().addLast(new EchoClientHandler());
+                        }
+                    });
+
+            ChannelFuture f = b.connect().sync();
+            f.channel().closeFuture().sync();
+
+        } catch (Exception e) {
+
+            log.info("错误的信息为:{}", e.getMessage());
+
+        } finally {
+
+            group.shutdownGracefully().sync();
+        }
+
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        if (args.length != 2) {
+
+            log.info("Usage:" + EchoClient.class.getSimpleName() + "<host><port>");
+            return;
+        }
+
+        final String host = args[0];
+
+        final int port = Integer.parseInt(args[1]);
+
+        new EchoClient(host, port).start();
+
+    }
+
+
+}
