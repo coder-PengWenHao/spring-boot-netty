63b149bf3e27d4dd10009417867d60442f33dbfc
coderpwh <pwhfbc@outlook.com>
2021-11-24 16:04:38 +0800

    Add files via upload
    upload chater4 package

---
+++ chapter4/src/main/java/com/coderpwh/netty/chapter4/PlainOioServer.java
@@ -0,0 +1,60 @@
+package com.coderpwh.netty.chapter4;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+/**
+ * @author pengwenhao
+ * @date 2021/11/24
+ */
+@Slf4j
+public class PlainOioServer {
+
+
+    public void serve(int port) throws IOException {
+
+        final ServerSocket socket = new ServerSocket(port);
+
+        try {
+            for (; ; ) {
+
+                final Socket clientSocket = socket.accept();
+                log.info("accepted connection from" + clientSocket);
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        OutputStream out;
+                        try {
+                            out = clientSocket.getOutputStream();
+                            out.write("Hi!\r\n".getBytes(Charset.forName("UTF-8")));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        } finally {
+                            try {
+                                clientSocket.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+
+                    }
+                }).start();
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+}
---
+++ chapter4/src/test/java/com/coderpwh/netty/chapter4/Chapter4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderpwh.netty.chapter4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
